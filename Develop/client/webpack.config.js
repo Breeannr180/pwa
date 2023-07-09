6def16ea75b11f088bf48e302fc49ee1571a29ca
BreeAnn <breeannbond@gmail.com>
2023-07-09 12:56:34 -0700

    added plugins and module rules

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -10,21 +10,66 @@ module.exports = () => {
   return {
     mode: 'development',
     entry: {
-      main: './src/js/index.js',
+      main: './src/js/index.js',//entry point for main page
       install: './src/js/install.js'
     },
-    output: {
+    output: {//output for main page
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Webpack plugin that generates html file
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Just Another Text Editor",
+      }),
+      // Injects our custom service worker.
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      // Webpack plugin that generates manifest file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E.",
+        description: "Create notes with or without an internet connection!",
+        background_color: "#272822",
+        theme_color: "#272822",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
-      ],
+        {//loader for css
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {//loader for babel to use ES6
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],  
     },
   };
 };
