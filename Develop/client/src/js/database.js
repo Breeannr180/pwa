9e50f1257836cfb763a77fe99f4b0ecf3dc22d98
BreeAnn <breeannbond@gmail.com>
2023-07-09 12:22:58 -0700

    added db JS

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,22 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {console.log('putDb implemented');
+  const jatedb = await openDB('jate', 1);//creates connection to database and proper version
+  const tx = jatedb.transaction('jate', 'readwrite');//creates transaction
+  const store = tx.objectStore('jate');//opens object store
+  await store.put(content);//adds content to object store
+  await tx.done;//closes transaction
+  console.log('Data added to the database');
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {console.log('getDb implemented');
+  const jatedb = await openDB('jate', 1);//creates connection to database and proper version
+  const tx = jatedb.transaction('jate', 'readonly');//creates transaction
+  const store = tx.objectStore('jate');//opens object store
+  const content = await store.getAll();//gets all content from object store
+  await tx.done;//closes transaction
+  return content;
+};
 initdb();
